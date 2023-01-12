775b502d10eae886c15bcc2db25c52a0161f382e
ThihaThant <116776396+ThihaThant@users.noreply.github.com>
2023-01-13 04:02:40 +0800

    Add files via upload

--- Foods/App.js
+++ Foods/App.js
@@ -1,62 +1,81 @@
 import * as React from 'react';
 import { Text, View, StyleSheet, Image, ScrollView } from 'react-native';
-import {Entypo} from 'react-native-vector-icons';
+import { Entypo } from 'react-native-vector-icons';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack=createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function App() {
-
-
+function Placeholder() {
   return (
     <View style={{ backgroundColor: '#5cdb95', height: '100%' }}>
-      <NavigationContainer>
-        
       <View style={{ paddingTop: 50, flexDirection: 'row' }}>
-      <Entypo.Button name='menu' size={40} style={{padding:7}} onPress={''} backgroundColor='#5cdb95'></Entypo.Button>
+        <Entypo.Button name='menu' size={40} style={{ padding: 7 }} onPress={''} backgroundColor='#5cdb95'></Entypo.Button>
         <Text style={[styles.header, { flex: 3 }]}>
           Saves
           <Text style={{ color: '#edf5e1' }}>Foods</Text>
         </Text>
-        <Entypo.Button name='user' size={30} style={{padding:10}} onPress={''} backgroundColor='#5cdb95'></Entypo.Button>
+        <Entypo.Button name='user' size={30} style={{ padding: 10 }} onPress={''} backgroundColor='#5cdb95'></Entypo.Button>
       </View>
-      <View
-        style={{
-          alignItems:'center',
-          backgroundColor: 'rgba(237,245,225,0.8)',
-          margin: 10,
-          borderRadius: 5,
-          padding: 4,
-        }}>
-        <Text style={{ color: 'rgba(152,158,177,0.8)'}}>
-          Search SavesFoods
-        </Text>
-      </View>
-      <View style={styles.nav}>
-        <Text style={[styles.navObject, { color:'#35424a' }]}>Featured</Text>
-        <Text style={styles.navObject}>New</Text>
-        <Text style={styles.navObject}>Nearby</Text>
-        <Text style={styles.navObject}>Following</Text>
-      </View>
-      <View style={{ height: 3, backgroundColor: '#edf5e1', flexDirection: 'column', marginRight: 15, marginLeft: 15 }}><View style={{ height: 3, backgroundColor: '#35424a', flexDirection: 'column', width: 60 }}></View></View>
-      <View style={{ height: '60%', margin: 20 }}>
-      <ScrollView>
-          <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-            <View style={styles.items}></View>
-            <View style={styles.items}></View>
-            <View style={styles.items}></View>
-            <View style={styles.items}></View>
-            <View style={styles.items}></View>
-            <View style={styles.items}></View>
-            <View style={styles.items}></View>
-            <View style={styles.items}></View>
+      <View style={{ height: '80%',marginTop:20 }}>
+        <ScrollView>
+          <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' }}>
+            <View style={styles.items}><Entypo name='user' size={40} style={{ margin: 10 }}></Entypo><View style={{ flex: 1, margin: 10 }}><Text>Placeholder Text</Text><Text>Placeholder Text </Text></View></View>
+
+            <View style={styles.items}><Entypo name='user' size={40} style={{ margin: 10 }}></Entypo><View style={{ flex: 1, margin: 10 }}><Text>Placeholder Text</Text><Text>Placeholder Text </Text></View></View>
+
+            <View style={styles.items}><Entypo name='user' size={40} style={{ margin: 10 }}></Entypo><View style={{ flex: 1, margin: 10 }}><Text>Placeholder Text</Text><Text>Placeholder Text </Text></View></View>
+
+            <View style={styles.items}><Entypo name='user' size={40} style={{ margin: 10 }}></Entypo><View style={{ flex: 1, margin: 10 }}><Text>Placeholder Text</Text><Text>Placeholder Text </Text></View></View>
+
+            <View style={styles.items}><Entypo name='user' size={40} style={{ margin: 10 }}></Entypo><View style={{ flex: 1, margin: 10 }}><Text>Placeholder Text</Text><Text>Placeholder Text </Text></View></View>
+
           </View>
         </ScrollView>
       </View>
-      </NavigationContainer>
     </View>
+  )
+}
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        tabBarActiveTintColor: '#edf5e1',
+        tabBarInactiveTintColor: '#05386b',
+        tabBarActiveBackgroundColor: '#7fe1ad',
+        tabBarInactiveBackgroundColor: '#7fe1ad',
+        headerShown: false
+      }}
+        initialRouteName="foods">
+        <Tab.Screen name='Foods' component={Placeholder} options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name='leaf' color={color} size={size} />
+          },
+
+        }} />
+        <Tab.Screen name='Sell' component={Placeholder} options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name='shop' color={color} size={size} />
+          },
+
+        }} />
+        <Tab.Screen name='Notifications' component={Placeholder} options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name='bell' color={color} size={size} />
+          },
+
+        }} />
+        <Tab.Screen name='Chat' component={Placeholder} options={{
+          tabBarIcon: ({ color, size }) => {
+            return <Entypo name='chat' color={color} size={size} />
+          },
+
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -69,7 +88,7 @@ const styles = StyleSheet.create({
     paddingRight: 5
   },
   navObject: {
-    color: '#edf5e1',
+    color: '#35424a',
     fontWeight: 'bold',
     padding: 10,
   },
@@ -84,11 +103,10 @@ const styles = StyleSheet.create({
     height: 50
   },
   items: {
-    backgroundColor: '#35424a',
-    width: 120,
-    height: 120,
-    margin: 20,
-    borderColor: 'white',
-    borderWidth: 4
+    backgroundColor: '#a5e8bb',
+    width: "90%",
+    flexDirection: 'row',
+    paddingBottom:20,
+    margin:10
   }
 });
